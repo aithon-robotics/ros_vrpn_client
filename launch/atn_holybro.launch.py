c62568cad9b8136a6420aef69dbfb67a1d9c0a6c
Spir0u <28966599+Spir0u@users.noreply.github.com>
2023-11-20 18:35:35 +0100

    add vrpn_server_ip as param in launch file

---
+++ launch/atn_holybro.launch.py
@@ -0,0 +1,44 @@
+# Copyright 2023 ASL
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+
+from launch_ros.actions import Node
+
+import yaml
+
+object_names = ["Staecke"] #["atn_holybro"]
+
+
+def get_params(package, file, name):
+    path = os.path.join(get_package_share_directory(package) + file)
+
+    with open(path, "r") as file:
+        params = yaml.safe_load(file)[name]["ros__parameters"]
+    return params
+
+
+def generate_launch_description():
+
+    params = get_params(
+        "ros_vrpn", "/config/asl_vicon.yaml", "ros_vrpn_client"
+    )
+
+    nodes = []
+    for object in object_names:
+        params["object_name"] = object
+        params["vrpn_server_ip"] = "10.10.10.5"
+        nodes.append(
+            Node(
+                package="ros_vrpn",
+                executable="ros_vrpn_client",
+                namespace=object,
+                output="screen",
+                parameters=[params],
+            )
+        )
+
+    return LaunchDescription(nodes)
